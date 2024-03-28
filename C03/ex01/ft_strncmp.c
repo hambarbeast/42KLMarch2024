83319e3339cbba626f793967f238bd86d6a295c5
hambarbeast <79783935+hambarbeast@users.noreply.github.com>
2024-03-28 23:50:06 +0800

    Add files via upload

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mawal <mawal@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 15:29:08 by mawal             #+#    #+#             */
+/*   Updated: 2024/03/28 17:13:30 by mawal            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	i = 0;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0') && (i < n - 1))
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
+}
+/*
+int	main(void)
+{
+	char s1[] = "Helpo";
+	char s2[] = "Bello";
+	printf("%d", strncmp(s1, s2, 5));
+	printf("%d", ft_strncmp(s1, s2, 5));
+}*/
